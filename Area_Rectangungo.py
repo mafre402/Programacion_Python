6772fe02808eeef9421a57ef1be8ae563e9903c6
mafre402 <102600438+mafre402@users.noreply.github.com>
2025-05-11 15:37:18 -0400

    Add files via upload

---
+++ Area_Rectangungo.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario que ingrese el largo y el ancho
+largo = float(input("Ingrese el largo del rectángulo: "))
+ancho = float(input("Ingrese el ancho del rectángulo: "))
+
+# Calcular el área
+area = largo * ancho
+
+# Mostrar el resultado
+print(f"El área del rectángulo es: {area}")
